942e3dd4560f8842b6e2d03528e92346d265be58
SridharGeetha <ssridhar4858@gmail.com>
2025-08-10 18:58:20 +0530

    sliding window problems added

--- DSA_PATTERN/Main.java
+++ DSA_PATTERN/Main.java
@@ -1,6 +1,7 @@
 package DSA_PATTERN;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Main {
@@ -8,18 +9,55 @@ public class Main {
         // String s1 = "ab";
         // String s2 = "eidaooo";
         // System.out.println(SlidingWindow.checkPermutaion(s1, s2));
-        int[][] firstList = {{0,2},{5,10},{13,23},{24,25}};
-        int[][] secondList = {{1,5},{8,12},{15,24},{25,26}};
-        List<int[]> ans = TwoPointer.intervalIntersection(firstList,secondList);
+        // int[][] firstList = {{0,2},{5,10},{13,23},{24,25}};
+        // int[][] secondList = {{1,5},{8,12},{15,24},{25,26}};
+        // List<int[]> ans = TwoPointer.intervalIntersection(firstList,secondList);
         // System.out.println(ans);
         // for(int[] num : ans){
         //     System.out.print("["+num[0]+" "+num[1]+"] ");
         // }
-        int x = -1;
-        System.out.println(1-x);
+        String s = "aabcdaabc";
+        Solution sol = new Solution();
+        int res = sol.getLPSLength(s);
+        System.out.println(res);
+        // int x = -1;
+        // System.out.println(1-x);
     }
 
     
+}
+ class Pair {
+int a;
+int b;
+public Pair(int a, int b) {
+    this.a = a;
+    this.b = b;
+}
+
+    
+}
+class Solution {
+    int getLPSLength(String s) {
+        int n = s.length();
+        int window = n;
+        // code here
+        if(n==1)return 0;
+        if(n==2)return s.charAt(0)==s.charAt(1)?1:0;
+        if(n<3)return s.substring(0,2).equals(s.substring(1,3))?2:s.substring(0,1).equals(s.substring(2,3))?1:0;
+        while (window >= 0) {
+            if (window < n) {
+                System.out.printf("{(%d ,%d) + (%d ,%d) [%d]} ", 0, window, n - window, n, window);
+                System.err.println(s.substring(0, window)+" "+(s.substring(n - window, n)));
+                if (s.substring(0, window).equals(s.substring(n - window, n))) {
+                    // System.out.println("found");
+                    return window;
+                }
+            }
+            window--;
+        }
+        // System.out.println();
+        return -1;
+    }
 }
 
 //[[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
--- DSA_PATTERN/SlidingWindow.java
+++ DSA_PATTERN/SlidingWindow.java
@@ -1,7 +1,11 @@
 package DSA_PATTERN;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.PriorityQueue;
+import java.util.Queue;
 
 //The sliding window technique is used when you're dealing with 
 //contiguous subarrays or substrings
@@ -15,6 +19,8 @@ import java.util.PriorityQueue;
 // Count of specific condition	Count of subarrays with sum < k, vowels, etc
 class SlidingWindow{
 
+    // <<<<<<<<<<<<<<<< Fixed Size >>>>>>>>>>>>>>>>>
+
      public int minimumDifference(int[] nums, int k) {
         int min = Integer.MAX_VALUE;
         int n = nums.length;
@@ -27,6 +33,26 @@ class SlidingWindow{
             right++;
         }
         return min;
+    }
+
+     public int numOfSubarrays(int[] arr, int k, int threshold) {
+     int sum = 0;
+     int cnt = 0;
+     int n = arr.length;
+     for(int i=0;i<k;i++)   {
+        sum += arr[i];
+     }
+     int left = 0;
+     int right = k-1;
+     while(right<n){
+        if(sum/k >= threshold) cnt++;
+        right++;
+        if(right<n){
+        sum += arr[right];
+        }
+        sum -= arr[left++];
+     }
+     return cnt;
     }
 
     public static boolean checkPermutaion(String s1, String s2) {
@@ -95,7 +121,7 @@ class SlidingWindow{
     }
 
     //Advanced Sliding window
-     public int[] maxSlidingWindow(int[] nums, int k) {
+    public int[] maxSlidingWindow(int[] nums, int k) {
         int n = nums.length;
         int[] ans = new int[n-k+1];
         int left = 1;
@@ -118,8 +144,72 @@ class SlidingWindow{
         return ans;
     }
 
+    public static List<Integer> firstNegInt(int arr[], int k) {
+        Queue<Integer> q = new LinkedList<>();
+        int ind = 0;
+        int n = arr.length;
+        while(ind <k && ind <n){
+            if(arr[ind]<0){
+                q.add(arr[ind]);
+            }
+            ind++;
+        }
+        int left = 1;
+        int right = k;
+        List<Integer> ans = new ArrayList<>();
+        ans.add(q.isEmpty() ? 0 : q.peek());
+        while(right < n){
+            if(arr[left-1]<0) q.poll();
+            if(arr[right]<0) q.add(arr[right]);
+            ans.add(q.isEmpty() ? 0 : q.peek());
+            left++;
+            right++;
+        }
+        return ans;
+    }
+
+
     //Need to solve Count Number of Nice Subarrays.
     //Max Consecutive Ones III
+    //Count of Anagrams 
+
+    
+    public static int longestSubarray(int[] nums) {
+        int n = nums.length;
+        int cnt = 0;
+        int left = 0;
+        int right = 0;
+        int zero = 0;
+        while(right<n){
+            if(nums[right] == 0) zero++;
+            if(zero > 1){
+                if(nums[left] == 0) zero--;
+                left++;
+            }
+            int val = zero == 0 ? right-left+1 : right - left;
+            if(zero<=1){
+                cnt = Math.max(cnt,val);
+            }
+            right++;
+        }
+        return cnt == n ? cnt-1 : cnt;
+    }
+
+     public static int longestSubarray_Clean_Code(int[] nums) {
+        int leftIndex = 0;
+        int zeroCount = 0;
+        for (int i = 0; i < nums.length; i++) {
+            
+            if (nums[i] == 0) zeroCount++;
+            
+            if (zeroCount > 1) {
+                if (nums[leftIndex] == 0) zeroCount--;
+                leftIndex++;
+            }
+        }
+        
+        return nums.length - 1 - leftIndex;
+    }
     //Longest Substring Without Repeating Characters
     //K-length Substrings with No Repeats
 
